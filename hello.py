f7de8039f60ee3b182e1480e70d1705e8c5ab43b
geethikasaisree <geethika.skit@gmail.com>
2023-11-27 12:13:04 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,21 @@
 print('Hello', end='')
 print('world')
-       
+
+a=3
+b=3.14
+
+d=int(3.14)
+print (type(d))
+
+e=str(d)
+print (type(e))
+
+print('Mayukha', end= ' ')
+print('sungeet')
+
+print(a,b)
+
+print(a,b, sep= '\n')
+print(a,b, sep= '@')
+
+print(a+b)
